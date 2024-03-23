6e7b6b488d1906791e92ecfad32195769ea80e36
npyl <e-radio-fk@gmail.com>
2024-03-23 10:47:39 +0200

    ok.

--- src/components/NavigationBar/Main.tsx
+++ src/components/NavigationBar/Main.tsx
@@ -1,44 +1,17 @@
 // @mui
 import { Box, BoxProps, Container } from "@mui/material";
-// hooks
-import useResponsive from "@/hooks/useResponsive";
-// config
-
-// components
-
-import { useSettingsContext } from "@/contexts/settings";
-import { HEADER, NAV } from "./config";
-
-// ----------------------------------------------------------------------
-
-const SPACING = 8;
 
 export default function Main({ children, sx, ...other }: BoxProps) {
-    const { themeLayout } = useSettingsContext();
-
-    const isNavMini = themeLayout === "mini";
-
-    const isDesktop = useResponsive("up", "lg");
-
     return (
         <Box
             component="main"
             sx={{
                 flexGrow: 1,
-                py: `${HEADER.H_MOBILE + SPACING}px`,
-                ...(isDesktop && {
-                    px: 2,
-                    py: `${HEADER.H_DASHBOARD_DESKTOP + SPACING}px`,
-                    width: `calc(100% - ${NAV.W_DASHBOARD}px)`,
-                    ...(isNavMini && {
-                        width: `calc(100% - ${NAV.W_DASHBOARD_MINI}px)`,
-                    }),
-                }),
                 ...sx,
             }}
             {...other}
         >
-            <Container>{children}</Container>
+            <Container maxWidth="xl">{children}</Container>
         </Box>
     );
 }

c78e9eb28a2feec0af2ced4a3d1102f3bb673daa
npyl <e-radio-fk@gmail.com>
2024-04-04 22:41:58 +0300

    route.ts

--- src/app/api/book/route.ts
+++ src/app/api/book/route.ts
@@ -1,1 +1,23 @@
 // TODO: ...
+
+import { NextResponse } from "next/server";
+
+export async function POST() {
+    try {
+        console.log("book: POST");
+
+        return new NextResponse(
+            JSON.stringify({ message: "Not implemented!" }),
+            {
+                status: 400,
+                headers: { "Content-Type": "application/json" },
+            },
+        );
+    } catch (error) {
+        console.error(error);
+        return new NextResponse(JSON.stringify(error), {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+        });
+    }
+}

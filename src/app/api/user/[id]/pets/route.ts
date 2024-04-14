b2910360367ba684572e3ce1b5835e05e3323b86
npyl <e-radio-fk@gmail.com>
2024-04-14 10:43:13 +0300

    Many-to-One between Pet and User

---
+++ src/app/api/user/[id]/pets/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../_util/db";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(
+    req: Request | NextRequest,
+    { params }: { params: { id: number } },
+) {
+    const { id } = params || {};
+
+    try {
+        if (!id) throw "Did not pass a user id!";
+
+        const petsForUserId = await prisma.pets.findMany({
+            where: {
+                ownerId: {
+                    equals: +id,
+                },
+            },
+        });
+
+        if (!Array.isArray(petsForUserId)) throw "Failure searching for pets.";
+
+        return new NextResponse(JSON.stringify(petsForUserId), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+        });
+    } catch (error) {
+        console.error(error);
+        return new NextResponse(JSON.stringify(error), {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+        });
+    }
+}
